c88de036de928ec90db77a7e5fedd8f42774e20b
Mustafa Mohamed <mustafa.qadir.mohamed@gmail.com>
2022-03-29 12:33:32 -0600

    Mustafa initial UI (#11)
    * students and organiztion page
    
    * students and organiztion page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,9 @@ function App() {
       <nav
         className='App-header'>
         <Link to="/login">Login</Link> | {' '}
-        <Link to="/register">Register</Link>
+        <Link to="/register">Register</Link> | {' '}
+        <Link to="/student">Student</Link> | {' '}
+        <Link to="/organization">Organization</Link>
       </nav>
     </div>
   );
