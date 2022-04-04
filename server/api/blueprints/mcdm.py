9b7e5700cd0e7a885b1d766ad7ad0b31fd685333
unknown <mamohame@ualberta.ca>
2022-04-04 12:16:58 -0600

    deleted the extra file and deleted the package.lock

--- server/api/blueprints/mcdm.py
+++ server/api/blueprints/mcdm.py
@@ -1,206 +1,36 @@
 from flask import Blueprint, request
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_api import status
-from json.tool import main
-import matplotlib.pyplot as plt
 
-class Mcdm:
-    """m = Mcdm(opts)
-    Multicriteria decision making aid
-    following the 'Pugh' scoring approach
-    starting with 'opts' as a tuple of option
-    identifiers.
-    Example:
-      m = Mcdm(('Car', 'Bus', 'Train'))"""
+class algorithm:
 
-    def __init__(self,opts):
-        self.options = opts
-        self.scores = {}
-        self._criteria = []
-    
-    def copy(self,reset_scores=False):
-        new_mcdm = Mcdm(self.options)
-        for (opt,cri) in self.scores.keys():
-            new_val = 0.0
-            if not reset_scores:
-                new_val = self.get_score(opt,cri)
-            new_mcdm.set_score(opt,cri,new_val)
-        return(new_mcdm)
-    
-    def __add__(self,other):
-        new_mcdm = self.copy()
-        for (opt,cri) in other.scores.keys():
-            assert (opt in self.options), "No such option: {}".format(opt)
-            self_val = self.get_score(opt,cri)
-            add_val = other.get_score(opt,cri)
-            new_mcdm.set_score(opt,cri,self_val+add_val)
-        return(new_mcdm)        
-    
-    def set_score(self,opt,cri,val):
-        """m.set_score(opt,cri,val)
-        Set score for option against criterion.
-        Will introduce new criterion if not scored
-        before, but will fail if option was not
-        included at initialization."""
-        assert (opt in self.options), "No such option: {}".format(opt)
-        self.scores[(opt,cri)]=val
-        # and add to critria list
-        if not cri in self._criteria:
-            self._criteria.append(cri)
-            
-    def set_scores_dict(self,score_dict):
-        """m.set_scores_dict(self,score_dict)
-        
-        Set multiple scores at once via dictionary.
-        Each key should be (opt,cri) and each value
-        should be a score.
-        Example:
-        
-        m.set_scores_dict({('Car','Speed'):1,('Car','Comfort'):-1})"""
-        for (opt,cri) in score_dict.keys():
-            assert (opt in self.options), "No such option: {}".format(opt)
-            self.set_score(opt,cri,score_dict[(opt,cri)])
-            
-    def criteria(self):
-        #return(set([cri for (opt,cri) in self.scores]))
-        return self._criteria
+    def __init__(self,inp):
+        self.ourCalculations = {}
+        self._grades = [] 
+        self.category = inp
 
-    def get_score(self,opt,cri):
-        assert (opt in self.options), "No such option: {}".format(opt)
-        assert (cri in self.criteria()), "No such criterion: {}".format(cri)
-        val = 0.0
-        if (opt,cri) in self.scores.keys():
-            val = self.scores[(opt,cri)]
-        return(val)
+    def grades(self):
+        return self._grades
 
-    def max_score(self):
-        return(max([max([self.get_score(opt,cri) for opt in self.options])
- for cri in self.criteria()]))
-
-    def min_score(self):
-        return(min([min([self.get_score(opt,cri) for opt in self.options])
- for cri in self.criteria()]))
-
-    def _rescale_all(self):
-        """Rescale all scores linearly to range [0,1]"""
-        new_mcdm = self.copy()
-        mn = self.min_score()
-        rg = self.max_score() - mn
-        for opt in self.options:
-            for cri in self.criteria():
-                new_val = (self.get_score(opt,cri) - mn)/rg
-                new_mcdm.set_score(opt,cri,new_val)
-        return(new_mcdm)
-
-    def _rescale_each_col(self):
-        """Rescale each column's scores linearly
-        such that each column fills range [0,1]"""
-        new_mcdm = self.copy()
-        for cri in self.criteria():
-            mn = self.select_criteria([cri]).min_score()
-            rg = self.select_criteria([cri]).max_score() - mn
-            for opt in self.options:
-                new_val = (self.get_score(opt,cri) - mn)/rg
-                new_mcdm.set_score(opt,cri,new_val)
-        return(new_mcdm)
-
-    def rescale(self,by_columns=False):
-        """Rescale all scores linearly to range [0,1]"""
-        if by_columns:
-            return(self._rescale_each_col())
+    def get_calculation(self,inputt,grade):
+        if (inputt,grade) in self.ourCalculations.keys():
+            a = self.ourCalculations[(inputt,grade)]
         else:
-            return(self._rescale_all())
+            a = 0
+        return(a)
+                
+    def set_calculation(self,a,inputt,grade):
+        self.ourCalculations[(inputt,grade)] = a
+        if not grade in self._grades:
+            self._grades.append(grade)
 
-    def weight_criteria(self,name,weights):
-        for cri in weights.keys():
-            assert cri in self.criteria(), "No such criterion: {}".format(cri)
-        for opt in self.options:
-            new_score = sum([weights[cri]*self.get_score(opt,cri) for cri in weights.keys()])
-            self.set_score(opt,name,new_score)
-
-    def weight_mixture(self, cri_list=None):
-        if cri_list is None:
-            cri_list=self.criteria()
-        for cri in cri_list:
-            assert cri in self.criteria(), "No such criterion: {}".format(cri)
-        num_cri = len(cri_list)
-        for cri in cri_list:
-            this_name = str(cri)+"_High"
-            this_weight = {c: 1.0/(1.0+num_cri) for c in cri_list}
-            this_weight[cri] = this_weight[cri]*2.0
-            self.weight_criteria(this_name,this_weight)
-
-    def select_criteria(self,cri_list):
-        for cri in cri_list:
-            assert cri in self.criteria(), "No such criterion: {}".format(cri)
-        new_mcdm = Mcdm(self.options)
-        for cri in cri_list:
-            for opt in self.options:
-                new_mcdm.set_score(opt,cri,self.get_score(opt,cri))
-        return(new_mcdm)
-    
-    def select_options(self,opt_list):
-        for opt in opt_list:
-            assert opt in self.options, "No such criterion: {}".format(opt)
-        new_mcdm = Mcdm(opt_list)
-        for cri in self.criteria():
-            for opt in opt_list:
-                new_mcdm.set_score(opt,cri,self.get_score(opt,cri))
-        return(new_mcdm)
-
-    def plot(self,show=True):
-        width=0.1
-        colors=['c','m','y','k','b','r','g']
-        fig,ax = plt.subplots()
-        for (ii,cri) in enumerate(self.criteria()):
-            cri_ind = [jj+ii*width for jj in range(len(self.options))]
-            cri_score = [self.get_score(opt,cri) for opt in self.options]
-            ax.bar(cri_ind,cri_score,width,color=colors[ii%len(colors)],label=str(cri))
-        ax.set_xticks([jj+0.5*ii*width for jj in range(len(self.options))])
-        ax.set_xticklabels(self.options)
-        ax.legend()
-        if show:
-            plt.show()
-        return(ax)
-
-    def __repr__(self):
-        opt_fmt='{:8}'
-        cri_name_fmt='{:>8.8}'
-        cri_val_fmt='{:8.3f}'
-        sep = ' : '
-        header = opt_fmt.format('Option') + sep + sep.join([cri_name_fmt.format(cri) for cri in self.criteria()]) + '\n'
-        rows = '\n'.join([opt_fmt.format(opt) + sep + sep.join([cri_val_fmt.format(self.get_score(opt,cri)) for cri in self.criteria()]) for opt in self.options]) + '\n'
-        row_sep = '='*8 + sep + sep.join(['='*8 for cri in self.criteria()]) + '\n'
-        return(header+row_sep+rows+row_sep)
-
-    def _raw_html(self):
-        table_start = '<table>\n'
-        header = '<tr><th>{}</th>'.format('Option') + ' '.join(['<th>{}</th>'.format(cri) for cri in self.criteria()]) + '</tr>\n'
-        rows = '\n'.join(['<tr><th>{}</th>'.format(opt) + ' '.join(['<td>{:8.3f}</td>'.format(self.get_score(opt,cri)) for cri in self.criteria()]) + '</tr>' for opt in self.options])
-        table_stop = '\n</table>\n'
-        return(table_start+header+rows+table_stop)
-
-    # def to_html(self):
-    #     return(HTML(self._raw_html()))
-    
-def travel_example():
-    travel = Mcdm(('Car','Bus','Train'))
-    travel.set_score('Car','Fuel',-1.0)
-    travel.set_score('Train','Price',1.0)
-    print(travel)
-    travel.rescale()
-    print(travel)
-    travel.weight_criteria('Intuit',{'Fuel': 1.0, 'Price': 0.1})
-    print(travel)
-    travel.weight_mixture(['Fuel','Price'])
-    print(travel)
-    print(travel.select_criteria(['Fuel_High','Price_High','Intuit']))
-    travel.select_criteria(['Fuel_High','Price_High','Intuit']).plot()
-    return(travel)
-
-if __name__=='__main__':
-    t=travel_example()
-
+    def set_weight(self,weights, studentID):
+        for inputt in self.category:
+            calculated_score = sum([weights[grade]*self.get_calculation(inputt,grade) for grade in weights.keys()])
+            self.set_calculation(calculated_score, inputt,studentID)
+            
+    def setDictionary(self,calculation_array):
+        for (inputt,grade) in calculation_array.keys():
+            self.set_calculation(calculation_array[(inputt,grade)], inputt,grade)
 
 mcdm = Blueprint("mcdm", __name__, url_prefix="/mcdm")
 
@@ -238,7 +68,7 @@ def run_mcdm():
     judge_score = form.get("judge_score")
     weight_criteria = form.get("weight_criteria")
 
-    bestStudent = Mcdm(('base', student_id))
+    bestStudent = algorithm(('base', student_id))
     
     studentDict = {
     ('base','acedemic'):1,
@@ -248,11 +78,11 @@ def run_mcdm():
     (student_id,'leadership'):judge_score['leadership'],
     (student_id,'volunteer'):judge_score['volunteer']
     }
-    bestStudent.set_scores_dict(studentDict)
-    bestStudent.weight_criteria('Intuition',{'acedemic':weight_criteria['academic'],'leadership':weight_criteria['leadership'],
-    'volunteer':weight_criteria['volunteer']})
+    bestStudent.setDictionary(studentDict)
+    bestStudent.set_weight({'acedemic':weight_criteria['academic'],'leadership':weight_criteria['leadership'],
+    'volunteer':weight_criteria['volunteer']}, 'Grade')
     
-    student_total_score = bestStudent.get_score(student_id, 'Intuition')
+    student_total_score = bestStudent.get_calculation(student_id, 'Grade')
 
 
     # success
